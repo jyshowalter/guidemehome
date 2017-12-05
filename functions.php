377c41931dc86f77119498ff63e8be096c1dcc8e
jyshowalter <33525333+jyshowalter@users.noreply.github.com>
2017-12-05 14:04:36 -0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,10 +2,14 @@ add_filter('gform_register_init_scripts', 'gform_radioNext');
 
 function gform_radioNext($form) {
 $script = '(function($){' .
-'$(".gfield_radio label").each(function(){' .
-'$(this).click(function(event) {' .
-'console.log(event.eventTarget);' .
-'});' .
+'$(".gfield_radio").each(function(){' .
+'var parent = $(this).closest(".gform_page");' .
+'var footer = $(".gform_page_footer", parent);' .
+'var next = $("input.gform_next_button", footer);' .
+'next.css("display", "none");' .
+'$("input[type=radio]", this).change(function(){' .
+'next.trigger("click");' .
+'})' .
 '})' .
 '})(jQuery);';
 
